304298d4519d9d4e2c3a0093eafa3f0e4a09bd5e
Elton Leite <eltonrms.leite@hotmail.com>
2024-05-14 08:50:33 -0300

    feat: adiciona o consumo de duas versões de uma lib web componentes na mesma página

--- projects/shell/src/main.ts
+++ projects/shell/src/main.ts
@@ -2,6 +2,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+// @ts-ignore
+import { MyElement } from 'lib-web-components';
+customElements.define('my-element-1', MyElement);
+
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
